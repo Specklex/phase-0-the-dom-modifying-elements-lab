57091a5d87f4d33216cf071706760c6f05e2a935
Jaden Lund <lundjaden@gmail.com>
2022-07-10 01:14:46 -0500

    commit tree d5427b66f35fc907abd3bad56da962bd9b62837d

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+//Step 1
+main.remove();
+
+//Step 2
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "is the champion";
+
